739d77017d877ec61b286bd04c202301076b45e8
peper11111 <piotr.pleban@interia.pl>
2015-06-04 13:38:42 +0200

    First brief of new look, ssl, new templates, jquery

--- foRtReSS.py
+++ foRtReSS.py
@@ -3,7 +3,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from contextlib import closing
 
 DATABASE = 'database/foRtReSS.db'
-DEBUG = True
 SECRET_KEY = 'development.key'
 
 app = Flask(__name__)
@@ -27,8 +26,13 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
-        
+
 @app.route('/')
+@app.route('/home')
+def home():
+    return show_entries()
+
+@app.route('/show_entries')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
@@ -43,49 +47,64 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
-
-@app.route('/create', methods=['GET', 'POST'])
-def create():
-    error = None
-    if request.method == 'POST':
-        if request.form['password'] != request.form['repassword']:
-            error = "Different passwords"
-        else:
-            g.db.execute('insert into users (username, password) values (?, ?)',
-                 [request.form['username'], request.form['password']])
-            g.db.commit()
-            session['logged_in'] = True
-            flash('New account created')
-            return redirect(url_for('show_entries'))
-    return render_template('create.html', error=error)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+    
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
     error = None
     if request.method == 'POST':
         cur = g.db.execute('select username, password from users order by id desc')
         entries = [dict(username=row[0], password=row[1]) for row in cur.fetchall()]
-        length = len(entries[0])
-        for i in range (0, length - 1):
-            if entries[i]['username'] == request.form['username']:
+        length = len(entries)
+        for i in range (0, length + 1):
+            try:
+                if entries[i]['username'] == request.form['username']:
+                    break
+            except IndexError:
+                i += 1
                 break
-        
-        if i == 2:
+
+        if i > length:
             error = 'Invalid username'
         else:
             if request.form['password'] != entries[i]['password']:
                 error = 'Invalid password'
             else:
                 session['logged_in'] = True
+                session['logged_user'] = request.form['username']
                 flash('You were logged in')
-                return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+                return redirect(url_for('home'))
+    return render_template('signin.html', error=error)
 
-@app.route('/logout')
-def logout():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] == '':
+            error = "Empty username field"
+        elif request.form['password'] == '':
+            error = "Empty password field"
+        elif request.form['password'] != request.form['repassword']:
+            error = "Passwords does not match"
+        else:
+            g.db.execute('insert into users (username, password) values (?, ?)',
+                 [request.form['username'], request.form['password']])
+            g.db.commit()
+            session['logged_in'] = True
+            session['logged_user'] = request.form['username']
+            flash('New account created')
+            return redirect(url_for('home'))
+    return render_template('signup.html', error=error)
+
+@app.route('/signout')
+def signout():
     session.pop('logged_in', None)
+    session.pop('logged_user', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('home'))
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=8000, debug=True, ssl_context=('certificate/server.crt', 'certificate/server.key'))
