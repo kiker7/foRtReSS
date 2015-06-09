f03f10e1787e822796b062b6d06d76352a4ea4c7
Peper <piotr.pleban@interia.pl>
2015-06-09 14:58:01 +0200

    LD

--- foRtReSS.py
+++ foRtReSS.py
@@ -330,11 +330,9 @@ def generate_csrf_token():
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='194.29.146.3', port=8000, debug=False, ssl_context=('certificate/server.crt', 'certificate/server.key'))
 =======
 # localhost
-    app.run(host='127.0.0.1', port=8000, debug=True, ssl_context=('certificate/server.crt', 'certificate/server.key'))
+#    app.run(host='127.0.0.1', port=8000, debug=True, ssl_context=('certificate/server.crt', 'certificate/server.key'))
 # volt
-#    app.run(host='194.29.146.3', port=8000, debug=False, ssl_context=('certificate/server.crt', 'certificate/server.key'))
->>>>>>> e14ceb721ee0f85e8e92d49037a968687d69fb0a
+    app.run(host='194.29.146.3', port=8000, debug=False, ssl_context=('certificate/server.crt', 'certificate/server.key'))
